fba750ae6af8464ad30b150b143a429b04d70d01
Thermacon <30552077+Thermacon@users.noreply.github.com>
2019-07-12 16:34:23 -0400

    Revamp of Renderer.lua
    Made code pretty, added comments, fixed up some timing, and changed a few variable names.

--- ServerScriptService/Renderer.lua
+++ ServerScriptService/Renderer.lua
@@ -1,99 +1,114 @@
+--Game services
 local httpService = game:GetService("HttpService")
 local sss = game:GetService("ServerScriptService")
 
+--Modules
 local opapi = require(sss.Modules.OverpassAPI)
 local partDisplay = require(sss.Modules.PartDisplaySystem)
 local stylesheet = require(sss.Modules.Stylesheet)
 local hex2rgb = require(sss.Modules.HexColorSupport)
 
-local loadRange = .01
-local lon = -73.988397
-local lat = 40.754024
+--Render settings
+local loadRange = .01 --How far to load
+local lon = -73.988397 --Longitude to load
+local lat = 40.754024 --Latitude to load
+
+local mpd = 111034 --Meters/Degree - Real world number that describes ~ how many meters in a degree
+local spm = 2.5 --Studs/Meter - How many studs fit in a meter (2.5 for ~ real world scaling)
+
+local delayCount = 10 --How many nodes/ways to complete before a wait()
+
+--Quick math...
 local minLat = lat - (loadRange/2)
 local minLon = lon - (loadRange/2)
 local maxLat = lat + (loadRange/2)
 local maxLon = lon + (loadRange/2)
 
-game.ReplicatedStorage.LoadingStatus:FireAllClients(true, "Requesting map nodes...", 0)
+--Get map's node data
 local query = opapi.query("node("..minLat..","..minLon..","..maxLat..","..maxLon.."); out;")
---local query = game.Workspace.N.Value
 local data = httpService:JSONDecode(query)
-local nodes = data["elements"]
-for index, node in pairs(nodes) do
-	wait()
-	local part = Instance.new("Part")
-	part.Anchored = true
-	part.Name = node.id
-	part.Parent = game.Workspace.Nodes
-	part.Material = Enum.Material.Glass
-	part.Size = Vector3.new(1, .1, 1)
+local mapNodes = data["elements"]
+
+for a, node in pairs(mapNodes) do --Loop through all nodes
+	if a%delayCount then wait() end --Wait every delayCount nodes
+
+	local nodeDisplay = Instance.new("Part") --Create node display part
+	nodeDisplay.Anchored = true
+	nodeDisplay.Name = node.id
+	nodeDisplay.Parent = game.Workspace.Nodes
+	nodeDisplay.Material = Enum.Material.Glass
+	nodeDisplay.Size = Vector3.new(.1, .1, .1)
 	
 	local localLat = node.lat-lat
 	local localLon = node.lon-lon
 	
-	part.CFrame = CFrame.new(localLat*111034*2.5, .05, localLon*111034*2.5)
-	game.ReplicatedStorage.LoadingStatus:FireAllClients(true, "Node "..node.id, index/#nodes, part.Position)
+	part.CFrame = CFrame.new(localLat*mpd*spm, .05, localLon*mpd*spm)
 end
 
-game.ReplicatedStorage.LoadingStatus:FireAllClients(true, "Requesting map ways...", 0)
+--Get map's ways data
 local query = opapi.query("way("..minLat..","..minLon..","..maxLat..","..maxLon.."); out;")
 local data = httpService:JSONDecode(query)
-local ways = data["elements"]
-for index, way in pairs(ways) do
-	local wayNodes = way.nodes
-	local tags = way.tags or {empty = "Nothing"}
+local mapWays = data["elements"]
+
+for a, way in pairs(mapWays) do --Loop through all nodes
+	wait()
 	
-	game.ReplicatedStorage.LoadingStatus:FireAllClients(true, tags.name or way.id, index/#ways)
+	local nodes = way.nodes --Get the current way's nodes
+	local tags = way.tags or {empty = "Nothing"} --Get the current way's tags, or nothing
+	
 	local style = stylesheet.getStyle(tags)
-	if tags["levels"] then
-		print(tags.levels)
-	end
-	for iindex, node in pairs(wayNodes) do
-		wait()
-		local partA = game.Workspace.Nodes:FindFirstChild(node)
+
+	for b, node in pairs(wayNodes) do
+		if b%delayCount then wait() end --Wait every delayCount nodes
+		
+		local partA = game.Workspace.Nodes:FindFirstChild(node) --PartA is always the current node
 		local partB
 		
-		if iindex ~= #wayNodes then
-			partB = game.Workspace.Nodes:FindFirstChild(wayNodes[iindex+1])
-		else
-			if style.loops then
-				partB = game.Workspace.Nodes:FindFirstChild(wayNodes[1])
+		--Find out what PartB is
+		if b ~= #wayNodes+1 then --If B is still in the array
+			partB = game.Workspace.Nodes:FindFirstChild(wayNodes[b+1]) --Get the next node
+		else --If B is the last node
+			if style.loops then --If B is not in the array
+				partB = game.Workspace.Nodes:FindFirstChild(wayNodes[1]) --Get the first node
 			else
-				partB = game.Workspace.Nodes:FindFirstChild(wayNodes[iindex])
+				partB = game.Workspace.Nodes:FindFirstChild(wayNodes[a]) --Get PartA as PartB
 			end
 		end
-		local height = 1
-		if tags["building:levels"] or tags["height"] then
-			if tags["height"] then
-				height = tags["height"]*2.5
-			else
-				height = tags["building:levels"]*7.5
+		
+		--Find out what height should be used
+		local height = 0
+		if tags["building:levels"] or tags["height"] then --If a tag exists that defins a height
+			if tags["height"] then --Tag "height" trumps tag "building:levels"
+				height = tags["height"]*spm
+			elseif tags["building:levels"] then
+				height = tags["building:levels"]*(spm*3)
 			end
-		else
+		end
+		if height == 0 then --If there still is not a height, use the default style one
 			height = style.height
 		end
+		
+		--Display the line
 		local line
-		if partA and partB then
-			line = partDisplay.makeLine(game.Workspace, partA.Position, partB.Position, height, style.width)
+		if partA and partB then --Make sure that partA and partB exist
+			line = partDisplay.makeLine(game.Workspace, partA.Position, partB.Position, height, style.width) --Draw the line
+			
+			--Set the styles
 			line.Material = style.material
-			if tags["building:colour"] then
+			line.Transparency = style.transparency
+			if tags["building:colour"] then --If the building:colour tag exists, use that color
 				line.BrickColor = BrickColor.new(hex2rgb(string.gsub(tags["building:colour"], "#", "")))
-			else
+			else --Or not...
 				line.BrickColor = style.color
 			end
-			line.Transparency = style.transparency
-			
-			game.ReplicatedStorage.LoadingStatus:FireAllClients(true, tags.name or way.id, index/#ways, partB.Position)
 		end
 	end
 end
 
+--Remove all the nodes, they are just a waste of parts now
 local oldNodes = game.Workspace.Nodes:GetChildren()
-
-for index, node in pairs(oldNodes) do
-	game.ReplicatedStorage.LoadingStatus:FireAllClients(true, "Removing nodes", index/#oldNodes)
+for a, node in pairs(oldNodes) do
 	node:Destroy()
 end
 
-game.ReplicatedStorage.LoadingStatus:FireAllClients(false, "Done!", 1)
 print("Done!")
